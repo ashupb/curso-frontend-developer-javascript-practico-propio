113277ad820f05930c428bc1c0b68db101e04557
Ashu PB <aperezbenary@gmail.com>
2023-05-31 14:38:14 -0300

    Integración del carrito de compras y arreglo de superposción entre menúes

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', togglemobileMenu);
+menuCarritoIcon.addEventListener('click', toggleMenuCarritoIcon);
 
 function toggleDesktopMenu() {
-    console.log('click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS');
+function togglemobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuCarritoIcon() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+  
+    aside.classList.toggle('inactive');
+}
